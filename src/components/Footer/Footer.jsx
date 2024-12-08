6500fd66b4a647a6ae6a422bab8ce402241f9bf7
Chathuranga <hmc.prasad999@gmail.com>
2024-12-08 23:35:12 +0530

    responsive added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className="footer" id='footer'>
             <div className="footer-content">
                 <div className="footer-content-left">
-                    <img src={assets.logo} alt="" />
+                    <img src={assets.logo} alt="" className='logo' />
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae dolore, voluptates laboriosam quaerat commodi illo odit amet! Quos excepturi aut inventore, sunt beatae quibusdam eveniet molestias, quaerat, aliquam facilis accusamus.</p>
                     <div className="footer-social-icons">
                         <img src={assets.facebook_icon} alt="" />
